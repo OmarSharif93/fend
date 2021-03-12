ccf7a201a273f037f6e430bcb247830c2b753649
OmarSharif93 <omar.shreef@outlook.com>
2021-03-12 18:37:35 +0200

    landing page 2021

--- projects/landing-page/README.md
+++ projects/landing-page/README.md
@@ -1,13 +1,11 @@
-# Landing Page Project
+# About
+this project is about creating a landing page with a dynamic Navigation-bar by using javascript manipulating the DOM tree.
 
-## Table of Contents
+# What We Used
+* Loops (for...of) (forEach)
+* DOM object methods (document.querySelectorAll()) 
+* documentFragment to enhance performance 
+* IntersectionObserver Interface to change active class for the current section in the view port 
+* events to interact with the users 
 
-* [Instructions](#instructions)
 
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -13,45 +13,114 @@
  * 
 */
 
-/**
- * Define Global Variables
- * 
-*/
+
+//Define Global Variables
+//Selecting all the Sections 
+const sections = document.querySelectorAll('section');
+
+//Making a Document Fragment
+const fragment = document.createDocumentFragment();
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+// End Global Variables
 
 
+//Start Helper Functions
+//Remove class 'your-active-class' from all sections
+function removeActiveClass() {
+    sections.forEach(section => {
+        section.classList.remove('your-active-class');
+    })
+}
+// End Helper Functions
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
 
+//Begin Main Functions
 // build the nav
+function buildNav() {
+
+    //Getting the first list 
+    const list = document.querySelector('ul');
+
+    for (const section of sections) {
+        //Making const that holds the 'data' value   
+        const anchorText = section.getAttribute('data-nav');
+
+        //Making const that holds the 'id' value 
+        const anchorLink = section.getAttribute('id');
+
+        //Creating 'li' element 
+        const listItem = document.createElement('li');
+
+        //Creating an Anchor Element
+        const aElement = document.createElement('a');
+
+        //Populating the 'a' Element
+        //add text
+        aElement.text = anchorText;
+        //add Link
+        aElement.setAttribute('href', '#' + anchorLink);
+        //add style class
+        aElement.setAttribute('class', 'menu__link')
+
+
+        //Appending the 'a' element to the 'li' element
+        listItem.appendChild(aElement);
+
+        //Appending the 'li' elements to the Document fragment
+        fragment.appendChild(listItem);
+    }
+    //Appending the Document Fragment to the 
+    list.appendChild(fragment);
+}
 
 
 // Add class 'active' to section when near top of viewport
+function toggleActiveState() {
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                //Calling remove Active class
+                removeActiveClass();
 
+                //Add the active class to the current intersecting section
+                entry.target.classList.add('your-active-class');
+            }
+        })
+    }, { threshold: 0.8 });
 
-// Scroll to anchor ID using scrollTO event
+    //observing Sections
+    sections.forEach(section => {
+        observer.observe(section);
+    })
+}
 
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+//End Main Functions
 
 // Build menu 
+buildNav();
 
-// Scroll to section on link click
-
+//Events
 // Set sections as active
+window.addEventListener('scroll', toggleActiveState);
+
+//Smooth Scroll
+function scrollToSection(event) {
+    event.preventDefault();
+    //Getting the Current Section
+    sections.forEach(section => {
+        if ('#' + section.getAttribute('id') === event.target.getAttribute('href')) {
+            //Smoothing the scroll
+            section.scrollIntoView({ behavior: "smooth", block: "center" });
+
+        }
+    })
+}
+
+//add Event Listener to every link
+const navLinks = document.querySelectorAll('a').forEach(navLink => {
+    navLink.addEventListener('click', scrollToSection);
+})
+
 
 
